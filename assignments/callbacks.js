c3bf51d005c613cf237e917997c55253808bf7ce
Mark Gowen <mark.gowen78@gmail.com>
2019-08-20 13:53:55 -0700

    completed second and third challenges in callbacks.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const school_items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 /* 
 
@@ -38,24 +38,36 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   console.log(test2); // "this Pencil is worth a million dollars!"
 */
 
-function getLength(items, callback) {
-  return callback(items);
-}
+// function getLength(items, callback) {
+//   return callback(items);
+// }
 
-const callback = items => {
-  return items.length;
-};
+// const callback = items => {
+//   return items.length;
+// };
 
-console.log(getLength(items, callback));
+// console.log(getLength(items, callback));
 
-function last(arr, cb) {
-  // last passes the last item of the array into the callback.
-}
+// function last(arr, callback) {
+//   // last passes the last item of the array into the callback.
+//   return callback(arr[arr.length-1]);
+// }
+
+// const callback = function callback(items) {
+//   return items
+// }
+
+// console.log(last(school_items, callback));
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x,y);
 }
 
+const add = (x, y) => {
+  return x + y;
+}
+console.log(sumNums(4, 5, add))
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
 }
